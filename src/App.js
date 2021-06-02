55205e06149c34692b8a2454b5e9f178b9f3527e
shiv2020s <kavi20900@gmail.com>
2021-06-02 14:29:46 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <div className="shopping-list">
+        <h1>Shopping List for</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+      <app-home>
+        
+      </app-home>
+      </header>
+    </div>
+  );
+
+}
+
+export default App;
---
+++ src/home.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="home">
+      <header className="home-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+    <h1>this is home</h1>
+      
+      </header>
+    </div>
+  );
+}
